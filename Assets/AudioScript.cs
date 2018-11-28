88d66f86cfabfd7156640569ee8d4040e8d72f24
dado3899 <dado3899@colorado.edu>
2018-11-27 19:50:47 -0700

    MusicScriptTest
    This is just the aspect which can edit music through script

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+    AudioSource myAudio;
+
+	// Use this for initialization
+	void Start () {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.clip = Resources.Load<AudioClip>("Spear Of Justice");
+        myAudio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/MixerLevels.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class MixerLevels : MonoBehaviour {
+
+    public AudioMixer MasterMixer;
+
+	// Use this for initialization
+	void Start () {
+        MasterMixer.SetFloat("MasterVolume", -10);
+        MasterMixer.SetFloat("LowpassFreq", 1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
