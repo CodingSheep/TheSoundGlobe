8ff094a24ebc1d67ad8fb0917f72cb92aa2c90f4
Jarrod Raine <jarrodfraine@me.com>
2018-12-03 12:13:43 -0700

    Added Particle Systems for Bands 2-5. Grouped objects

---
+++ Assets/Scripts/ColorParticleOnFrequency.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorParticleOnFrequency : MonoBehaviour {
+
+    public int band;
+
+    ParticleSystem ps;
+
+    void Start () {
+        ps = GetComponent<ParticleSystem>();
+    }
+	
+	void Update () {
+        // Update Color based on Frequency band
+        var col = ps.colorOverLifetime;
+        col.enabled = true;
+        col.color = new Color(AudioCalculator.audioBandBuffer[band], AudioCalculator.audioBandBuffer[band], AudioCalculator.audioBandBuffer[band]);
+    }
+}
